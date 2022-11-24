9bd1c42dbe76604896dfcccf021d2a6fce74b276
Sourabh-40 <rathisourabh40@gmail.com>
2022-11-24 10:03:14 +0530

    hi
    hello

---
+++ MyGitHub/src/main/java/stGitHub/MyGitHub/App.java
@@ -0,0 +1,13 @@
+package stGitHub.MyGitHub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
